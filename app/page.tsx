4b1f3e48b0a34821c14ed8ac734ad82bd9af7b4d
Ghabriel Molina <ghabrielmolina@hotmail.com>
2024-05-01 15:22:51 -0300

    feat: add promo banner to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import CategoryList from "./_components/category-list";
 import Header from "./_components/header";
 import Search from "./_components/search";
@@ -12,6 +13,18 @@ const Home = () => {
 
       <div className="px-5 pt-6">
         <CategoryList />
+      </div>
+
+      <div className="px-5 pt-6">
+        <Image
+          src="/promo-banner-01.png"
+          alt="Até 30% de desconto em pizzas"
+          width={0}
+          height={0}
+          className="h-auto w-full object-contain" // object-contain para manter a proporção da imagem e preencher o espaço
+          sizes="100vw" // Tamanho da imagem em relação à tela quando não há um width definido
+          quality={100} // Qualidade da imagem
+        />
       </div>
     </>
   );
