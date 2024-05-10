ee35ad938e6f4ab2a5993002219bd52166f016aa
Ghabriel Molina <ghabrielmolina@hotmail.com>
2024-05-10 18:10:39 -0300

    feat: add cart banner component

--- app/_context/cart.tsx
+++ app/_context/cart.tsx
@@ -27,6 +27,7 @@ interface ICardContext {
   subtotalPrice: number;
   totalPrice: number;
   totalDiscounts: number;
+  totalQuantity: number;
   addProductToCart: ({
     product,
     quantity,
@@ -55,6 +56,7 @@ export const CartContext = createContext<ICardContext>({
   subtotalPrice: 0,
   totalPrice: 0,
   totalDiscounts: 0,
+  totalQuantity: 0,
   addProductToCart: () => {},
   decreaseProductQuantity: () => {},
   increaseProductQuantity: () => {},
@@ -79,6 +81,12 @@ export const CartProvider = ({ children }: { children: ReactNode }) => {
         return acc + calculateProductTotalPrice(product) * product.quantity;
       }, 0) + Number(products?.[0]?.restaurant?.deliveryFee)
     );
+  }, [products]);
+
+  const totalQuantity = useMemo(() => {
+    return products.reduce((acc, product) => {
+      return acc + product.quantity;
+    }, 0);
   }, [products]);
 
   const totalDiscounts =
@@ -180,6 +188,7 @@ export const CartProvider = ({ children }: { children: ReactNode }) => {
         subtotalPrice,
         totalPrice,
         totalDiscounts,
+        totalQuantity,
       }}
     >
       {children}
