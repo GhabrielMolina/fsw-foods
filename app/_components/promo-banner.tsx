6fffbbc78e05ade71bc493c8a89f69dbbe302e98
Ghabriel Molina <ghabrielmolina@hotmail.com>
2024-05-17 19:39:14 -0300

    feat: add sizes property to images

--- app/_components/promo-banner.tsx
+++ app/_components/promo-banner.tsx
@@ -6,7 +6,7 @@ const PromoBanner = (props: ImageProps) => {
       width={0}
       height={0}
       className="h-auto w-full object-contain" // object-contain para manter a proporção da imagem e preencher o espaço
-      sizes="100vw" // Tamanho da imagem em relação à tela quando não há um width definido
+      sizes="100%"
       quality={100} // Qualidade da imagem
       {...props}
     />
