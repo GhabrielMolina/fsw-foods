25fd80d5d757fc63b00a39a070d9116d7e527482
Ghabriel Molina <ghabrielmolina@hotmail.com>
2024-05-17 19:26:03 -0300

    refactor: remove memo from cart context

--- app/_context/cart.tsx
+++ app/_context/cart.tsx
@@ -23,30 +23,18 @@ export interface CartProduct
   quantity: number;
 }
 
-// Interface que define o formato do contexto do carrinho de compras (ICardContext) com um array de produtos (products) e uma função para adicionar produtos ao carrinho (addProductToCart)
-interface ICardContext {
+// Interface que define o formato do contexto do carrinho de compras (ICartContext) com um array de produtos (products) e uma função para adicionar produtos ao carrinho (addProductToCart)
+interface ICartContext {
   products: CartProduct[];
-  subTotalPrice: number;
+  subotalPrice: number;
   totalPrice: number;
   totalDiscounts: number;
   totalQuantity: number;
   addProductToCart: ({
     product,
-    quantity,
     emptyCart,
   }: {
-    product: Prisma.ProductGetPayload<{
-      include: {
-        restaurant: {
-          select: {
-            id: true;
-            deliveryTimeMinutes: true;
-            deliveryFee: true;
-          };
-        };
-      };
-    }>;
-    quantity: number;
+    product: CartProduct;
     emptyCart?: boolean;
   }) => void;
   decreaseProductQuantity: (productId: string) => void;
@@ -56,9 +44,9 @@ interface ICardContext {
 }
 
 // Criar o contexto do carrinho de compras com um valor inicial vazio e uma função vazia para adicionar produtos ao carrinho de compras (addProductToCart) e exportar o contexto
-export const CartContext = createContext<ICardContext>({
+export const CartContext = createContext<ICartContext>({
   products: [],
-  subTotalPrice: 0,
+  subotalPrice: 0,
   totalPrice: 0,
   totalDiscounts: 0,
   totalQuantity: 0,
@@ -74,36 +62,31 @@ export const CartProvider = ({ children }: { children: ReactNode }) => {
   const [products, setProducts] = useState<CartProduct[]>([]);
 
   // Calcular o preço total de um produto no carrinho de compras com base no preço total do produto e a quantidade do produto
-  const subTotalPrice = useMemo(() => {
-    return products.reduce((acc, product) => {
-      return acc + Number(product.price) * product.quantity;
-    }, 0);
-  }, [products]);
+  const subotalPrice = products.reduce((acc, product) => {
+    return acc + Number(product.price) * product.quantity;
+  }, 0);
 
   // Calcular o preço total dos produtos no carrinho de compras com base no preço total de cada produto e a quantidade de cada produto
-  const totalPrice = useMemo(() => {
-    return (
-      products.reduce((acc, product) => {
-        return acc + calculateProductTotalPrice(product) * product.quantity;
-      }, 0) + Number(products?.[0]?.restaurant?.deliveryFee)
-    );
-  }, [products]);
+  const totalPrice =
+    products.reduce((acc, product) => {
+      return acc + calculateProductTotalPrice(product) * product.quantity;
+    }, 0) + Number(products?.[0]?.restaurant?.deliveryFee);
 
-  const totalQuantity = useMemo(() => {
-    return products.reduce((acc, product) => {
-      return acc + product.quantity;
-    }, 0);
-  }, [products]);
+  const totalQuantity = products.reduce((acc, product) => {
+    return acc + product.quantity;
+  }, 0);
 
   const totalDiscounts =
-    subTotalPrice - totalPrice + Number(products?.[0]?.restaurant?.deliveryFee);
+    subotalPrice - totalPrice + Number(products?.[0]?.restaurant?.deliveryFee);
 
   const clearCart = () => {
     return setProducts([]);
   };
 
   // Função que diminui a quantidade de um produto no carrinho de compras e atualiza o estado dos produtos
-  const decreaseProductQuantity = (productId: string) => {
+  const decreaseProductQuantity: ICartContext["decreaseProductQuantity"] = (
+    productId: string,
+  ) => {
     return setProducts((prev) =>
       prev.map((cartProduct) => {
         if (cartProduct.id === productId) {
@@ -119,7 +102,9 @@ export const CartProvider = ({ children }: { children: ReactNode }) => {
   };
 
   // Função que aumenta a quantidade de um produto no carrinho de compras e atualiza o estado dos produtos
-  const increaseProductQuantity = (productId: string) => {
+  const increaseProductQuantity: ICartContext["increaseProductQuantity"] = (
+    productId: string,
+  ) => {
     return setProducts((prev) =>
       prev.map((cartProduct) => {
         if (cartProduct.id === productId) {
@@ -134,7 +119,9 @@ export const CartProvider = ({ children }: { children: ReactNode }) => {
   };
 
   // Função que remove um produto do carrinho de compras e atualiza o estado dos produtos
-  const removeProductFromCart = (productId: string) => {
+  const removeProductFromCart: ICartContext["removeProductFromCart"] = (
+    productId: string,
+  ) => {
     return setProducts((prev) =>
       // Filtrar os produtos para remover o produto com o id passado como argumento da função removeProductFromCart (productId) do array de produtos (prev) e retornar um novo array sem o produto removido
       prev.filter((product) => product.id !== productId),
@@ -142,24 +129,9 @@ export const CartProvider = ({ children }: { children: ReactNode }) => {
   };
 
   // Função que adiciona um produto ao carrinho de compras com uma quantidade inicial de 0 (zero) e atualiza o estado dos produtos
-  const addProductToCart = ({
+  const addProductToCart: ICartContext["addProductToCart"] = ({
     product,
-    quantity,
     emptyCart,
-  }: {
-    product: Prisma.ProductGetPayload<{
-      include: {
-        restaurant: {
-          select: {
-            id: true;
-            deliveryTimeMinutes: true;
-            deliveryFee: true;
-          };
-        };
-      };
-    }>;
-    quantity: number;
-    emptyCart?: boolean;
   }) => {
     if (emptyCart) {
       setProducts([]);
@@ -177,7 +149,7 @@ export const CartProvider = ({ children }: { children: ReactNode }) => {
           if (cartProduct.id === product.id) {
             return {
               ...cartProduct,
-              quantity: cartProduct.quantity + quantity,
+              quantity: cartProduct.quantity + product.quantity,
             };
           }
           return cartProduct;
@@ -185,7 +157,7 @@ export const CartProvider = ({ children }: { children: ReactNode }) => {
       );
     }
 
-    setProducts((prev) => [...prev, { ...product, quantity: quantity }]);
+    setProducts((prev) => [...prev, product]);
   };
 
   return (
@@ -197,7 +169,7 @@ export const CartProvider = ({ children }: { children: ReactNode }) => {
         decreaseProductQuantity,
         increaseProductQuantity,
         removeProductFromCart,
-        subTotalPrice,
+        subotalPrice,
         totalPrice,
         totalDiscounts,
         totalQuantity,
